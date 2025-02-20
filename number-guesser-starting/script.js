41840fdd129a7bf717be824d011869d9dca6ee83
josueeliezer01 <eliezercasstilho@gmail.com>
2025-02-20 17:05:48 +0000

    First commit

---
+++ number-guesser-starting/script.js
@@ -0,0 +1,23 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 9);
+};
+
+const compareGuesses = (humanGuess, computerGuess, target) => {
+  const humanDifference = Math.abs(target - humanGuess);
+  const computerDifference = Math.abs(target - computerGuess);
+  return humanDifference <= computerDifference;
+};
+
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
